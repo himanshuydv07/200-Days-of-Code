409cfd7bd4e1f2d6bb1cc046f4e0b0ca7bf20618
himanshuydv07 <kukuydv07@gmail.com>
2026-07-20 21:29:55 +0530

    Add 3739. Count Subarrays With Majority Element II.cpp

---
+++ Day 125/3739. Count Subarrays With Majority Element II.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    struct Fenwick {
+        int n;
+        vector<long long> bit;
+        Fenwick(int n) {
+            this->n = n;
+            bit.assign(n + 1, 0);
+        }
+
+        void update(int idx, int val) {
+            while (idx <= n) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        long long query(int idx) {
+            long long res = 0;
+            while (idx > 0) {
+                res += bit[idx];
+                idx -= idx & -idx;
+            }
+            return res;
+        }
+    };
+
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> pref(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        vector<int> vals = pref;
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+
+        Fenwick ft(vals.size());
+
+        long long ans = 0;
+
+        for (int x : pref) {
+            int id = lower_bound(vals.begin(), vals.end(), x) - vals.begin() + 1;
+
+            // Count previous prefix sums strictly smaller than current
+            ans += ft.query(id - 1);
+
+            ft.update(id, 1);
+        }
+
+        return ans;
+    }
+};
